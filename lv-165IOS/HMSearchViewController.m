ca027773f6c7221a47bf7dc656a491a04fddb607
roman <roman.vovk.s@gmail.com>
2015-12-08 20:05:15 +0200

    For show navigation bar!

--- lv-165IOS/HMSearchViewController.m
+++ lv-165IOS/HMSearchViewController.m
@@ -20,6 +20,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [[self navigationController] setNavigationBarHidden:NO animated:YES];
     // Do any additional setup after loading the view.
 }
 #pragma mark - UISearchBarDelegate
